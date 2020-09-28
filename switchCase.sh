ae21f162ccecd15afd9080e8b25d8efdbac38fc4
panic-coder <androidshubham@gmail.com>
2020-09-28 20:38:23 +0530

    first commit

---
+++ switchCase.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+for filename in $(ls)
+do
+	ext=${filename##*\.}
+	case "$ext" in
+		java) echo "$filename : Java source file"
+			;;
+		sh) echo "$filename : Shell Script"
+			;;
+		txt) echo "$filename : Text file"
+			;;
+		*) echo "$filename : Default"
+			;;
+	esac
+done
+
